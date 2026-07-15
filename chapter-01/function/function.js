290065972715c2566a2e9e3e791a89a100663616
Davi Fernandes <davi.fer159@gmail.com>
2026-07-15 08:56:18 -0300

    functions

---
+++ chapter-01/function/function.js
@@ -0,0 +1,20 @@
+function sayHello() {
+  console.log("Hello World!");
+}
+
+sayHello();
+
+// Passando argumentos para uma função
+function output(text) {
+  console.log(text);
+}
+
+output("Hello World!");
+
+// Retornando valores
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+var result = sum(5, 4);
+output(result);
