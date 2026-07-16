78758536784cd73d4b9e15afd41f3fc5ccef6f57
Davi Fernandes <davi.fer159@gmail.com>
2026-07-16 08:53:23 -0300

    escope with let and const

---
+++ chapter-02/variables/escope.js
@@ -0,0 +1,29 @@
+let movie = "Lord of the Rings";
+// var movie = "Batman vs Superman"; - Identifier 'movie' has already been declared
+function starWarsFan() {
+  const movie = "Star Wars";
+  return movie;
+}
+
+function marvelFan() {
+  movie = "The Avengers";
+  return movie;
+}
+
+function blizzardFan() {
+  const isFan = true;
+  let phrase = "Warcraft";
+  console.log("Before if: " + phrase);
+  if (isFan) {
+    let phrase = "initial text";
+    phrase = "For the Horde!";
+    console.log("Inside if: " + phrase);
+  }
+  phrase = "For the Alliance!";
+  console.log("After if: " + phrase);
+}
+console.log(movie);
+console.log(starWarsFan());
+console.log(marvelFan());
+console.log(movie);
+blizzardFan();
