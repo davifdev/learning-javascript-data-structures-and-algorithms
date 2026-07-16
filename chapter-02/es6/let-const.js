73b0f1f554aa21aa61946c39b72b41a005d833de
Davi Fernandes <davi.fer159@gmail.com>
2026-07-16 08:37:00 -0300

    let and const

---
+++ chapter-02/es6/let-const.js
@@ -0,0 +1,18 @@
+var framework = "Angular";
+var framework = "React";
+
+console.log(framework);
+
+let language = "JavaScript";
+// let language = "Python";
+
+const PI = 3.14;
+// PI = 3.0;
+
+const jsFramework = {
+  name: "Angular",
+};
+
+jsFramework.name = "React";
+
+console.log(jsFramework.name);
