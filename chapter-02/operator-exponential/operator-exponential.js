29dae68f905d61daedeab93b1002ef02b5652b2a
Davi Fernandes <davi.fer159@gmail.com>
2026-07-16 10:51:19 -0300

    operator exponential

---
+++ chapter-02/operator-exponential/operator-exponential.js
@@ -0,0 +1,6 @@
+// O operador de exponencial pode ser conveniente quando trabalhamos com matemática
+const r = 3;
+
+const area = 3.14 * r * r;
+const area2 = 3.14 * Math.pow(r, 2);
+const area3 = 3.14 * r ** 2;
