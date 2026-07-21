b3926cdc4bd028e71cda7b0a7f5e70234041b812
Davi Fernandes <davi.fer159@gmail.com>
2026-07-21 12:16:32 -0300

    refactor: add explicit type annotation for numbers array in TypeScript

--- chapter-03/array/array-es6/array-typescript.ts
+++ chapter-03/array/array-es6/array-typescript.ts
@@ -1,8 +1,7 @@
 // Todo o código-fonte deste capítulo é um código TypeScript válido. A diferença é que o TypeScript fará verificação de tipos em tempo de compilação para garantir que estamos manipulando somente arrays nos quais todos os valores tenham o mesmo tipo de dado.
 
 // Por causa da inferência de tipo, o TypeScript entende que a declaração do array numbers é igual a const numbers: number[], por isso não precisamos declarar explicitamente o tipo da variável se ela for inicializada em sua declaração.
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 interface Person {
   name: string;
   age: number;
