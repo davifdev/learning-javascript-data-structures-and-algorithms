bdd2a6b3f2b3699e9f693303b5561a56d384a470
Davi Fernandes <davi.fer159@gmail.com>
2026-07-30 10:28:27 -0300

    linked list

--- chapter-06/linked-list.js
+++ chapter-06/linked-list.js
@@ -1,13 +1,11 @@
 import { defaultEquals } from "../utils/index.js";
 import { Node } from "../models/linked-list-models.js";
-
 export default class LinkedList {
   constructor(equalsFn = defaultEquals) {
     this.count = 0;
     this.head = undefined;
     this.equalsFn = equalsFn;
   }
-
   // Adiciona um novo elemento no final da lista
   push(element) {
     const node = new Node(element);
@@ -23,19 +21,17 @@ export default class LinkedList {
     }
     this.count++;
   }
-
   // Esse método devolve o elemento que está em uma posição específica da lista, se o elemento não estiver na lista ele retorna undefined.
   getElement(index) {
-    if (index >= 0 && index <= this.count) {
-      let node = this.head;
-      for (let i = 0; i < index && node !== null; i++) {
-        node = node.next;
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      for (let i = 0; i < index && current !== null; i++) {
+        current = current.next;
       }
-      return node;
+      return current;
     }
     return undefined;
   }
-
   // Esse método remove um elemento da lista
   removeAt(index) {
     if (index >= 0 && index < this.count) {
@@ -52,7 +48,6 @@ export default class LinkedList {
     }
     return undefined;
   }
-
   // Esse método insere um novo elemento em uma posição específica da lista.
   insert(element, index) {
     if (index >= 0 && index <= this.count) {
@@ -72,11 +67,10 @@ export default class LinkedList {
     }
     return false;
   }
-
   // Esse método devolve o índice do elemento da lista, se o elemento não estiver na lista -1 será devolvido
   indexOf(element) {
     let current = this.head;
-    for (let i = 0; i < this.count && currrent != null; i++) {
+    for (let i = 0; i < this.count && current != null; i++) {
       if (this.equalsFn(element, current.element)) {
         return i;
       }
@@ -84,27 +78,22 @@ export default class LinkedList {
     }
     return -1;
   }
-
   // Esse método remove um elemento de uma posição específica da lista
   remove(element) {
     const index = this.indexOf(element);
     return this.removeAt(index);
   }
-
   // Esse método devolve o número de elementos contidos na lista ligada. É semelhante á propiedade length do array
   size() {
     return this.count;
   }
-
   // Esse método devolve true se a lista ligada não contiver nenhum elemento e false se o tamanho da lista ligada for maior que 0
   isEmpty() {
     return this.size() === 0;
   }
-
   getHead() {
     return this.head;
   }
-
   // Esse método devolve uma representação em string da lista ligada.
   toString() {
     if (this.head == null) {
@@ -121,8 +110,8 @@ export default class LinkedList {
 }
 
 const list = new LinkedList();
-list.push(14);
-list.push(15);
-list.push(16);
-list.removeAt(1);
+list.push(10);
+list.push(5);
+list.push(3);
+console.log(list.indexOf(10));
 console.log(list.toString());
